773c8730d45cc418f78928e65fe4a67eea3350ea
virat-18-git <ksprddy6@gmail.com>
2024-12-20 16:23:12 +0530

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# practise
+variable:stores any value or inforamtion
+declaration:contains the name of the variable
+assigning:assigns value
+initialisation:both declaration + assigning
+
+
+undefined is not error its a king of data
---
+++ index.js
@@ -0,0 +1,46 @@
+
+// let a;
+// console.log(a) variable declaration
+// let a;
+// a=10;
+// console.log(a) variable declaration with assigning value
+// let a=25;
+// console.log(a) declaration
+
+
+
+// let a=100;
+// console.log(typeof a)
+
+
+// let a=100;
+// console.log(typeof typeof a)
+
+// a="100"
+// b=50;
+// console.log(a+b)
+
+
+// a="100"
+// b=50;
+// console.log(a-b)
+
+
+// a="100"
+// b=50;
+// console.log(a%b)
+
+
+// a="100"
+// b=50;
+// console.log(a/b)
+
+// a="100"
+// b=50;
+// console.log(a*b)
+
+// a="shiva"
+// b=50;
+// console.log(a*b)
+
+
